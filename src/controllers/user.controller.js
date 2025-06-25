2bcc3d1fe309cb7d5e9e48e40ebe16a2e394aaae
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2025-06-26 01:03:49 +0530

    Added resgister controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
-import ApiError from "../utils/apiError.js"
+import ApiError from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
+import {uploadOnCloud} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/apiResponse.js"
 
 const registerUser = asyncHandler( async(req, res) => {
     //get user detail from frontend
@@ -30,9 +32,43 @@ const registerUser = asyncHandler( async(req, res) => {
     if(existedUser){
         throw new ApiError(409, "User with email and username already exists");
     }
-    
 
-})
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    console.log(req.files);
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required");
+    }
+    const avatar = await uploadOnCloud(avatarLocalPath)
+    const coverImage = await uploadOnCloud(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar upload failed")
+    }
+
+    const user = await User.create({
+        fullName, 
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(createdUser){
+        throw new ApiError(500, "User registration failed")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
+
+})  
 
 
 export {registerUser}
